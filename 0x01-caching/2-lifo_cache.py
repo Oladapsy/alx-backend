e265ef8c5133f4a8562c0f6c4330666f03e3c1fb
oladapsy <dapoodedeyi@yahoo.com>
2024-10-29 16:21:58 +0000

    update

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+    Create a class LIFOCache that inherits
+    from BaseCaching and is a caching system:
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """a lif0 caching class"""
+    def __init__(self):
+        """the initialization method"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ put an item in cache"""
+        if key or item:
+            if key not in self.cache_data:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    new_key = self.order.pop()
+                    del self.cache_data[new_key]
+                    print(f"DISCARD: {new_key}")
+                self.order.append(key)
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            Must return the value in self.cache_data
+            linked to key.
+            If key is None or if the key doesn’t exist
+            in self.cache_data, return None.
+        """
+        if key is None:
+            return None
+
+        if key not in self.cache_data:
+            return None
+
+        return self.cache_data.get(key)
