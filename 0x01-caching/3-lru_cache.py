2706f7977bfbb64f8e76a8adee3b1ee4f312ff8c
oladapsy <dapoodedeyi@yahoo.com>
2024-10-30 09:17:12 +0000

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,15 +8,36 @@ from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
     """the least recently used caching"""
-    def __int__(self):
+    def __init__(self):
         """ the initialization overiding base"""
         super().__init__()
         self.order = []
 
     def put(self, key, item):
         """put an item in lru cache"""
-        if key and item:
-            if key in self.cache_data:
+        if key is None or item is None:
+            return
 
-            self.cache_data[key] = item
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = self.order.pop(0)
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """
+            Must return the value in self.cache_data linked to key.
+            If key is None or if the key doesn’t exist in
+            self.cache_data, return None.
+        """
+        if key not in self.cache_data:
+            return None
+        if key in self.order:
+            self.order.remove(key)
             self.order.append(key)
+        return self.cache_data.get(key)
