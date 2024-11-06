68d12a3d9274b531fcf77f342a36b03204f8d29d
oladapsy <dapoodedeyi@yahoo.com>
2024-11-06 21:28:48 +0000

    test

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python3
 """
-Setup a basic Flask app in 3-app.py
+    First you will setup a basic Flask app in 0-app.py.
+    Create a single / route and an index.html template
+    that simply outputs “Welcome to Holberton” as page
+    title (<title>) and “Hello world” as header (<h1>).
 """
-from flask import Flask, render_template, request
+from flask import Flask, request
+from flask import render_template
 from flask_babel import Babel
 
 
-class Config(object):
-    """
-    Config class for Babel’s default locale ("en")
-    and timezone ("UTC").
-    """
+class Config:
+    """change default config in flask babel"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """
-    Determine the best match with our supported languages.
+        to determine the best match with our supported languages.
     """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.Config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Returns the render of 3-index.html
+        the / root page
+        renders basic html
     """
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
