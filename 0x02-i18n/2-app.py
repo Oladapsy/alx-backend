fd8674e3297733f271c89cf53b0c9cf397cec597
oladapsy <dapoodedeyi@yahoo.com>
2024-11-06 05:48:40 +0000

    set locale_language

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+    First you will setup a basic Flask app in 0-app.py.
+    Create a single / route and an index.html template
+    that simply outputs “Welcome to Holberton” as page
+    title (<title>) and “Hello world” as header (<h1>).
+"""
+from flask import Flask, request
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config:
+    """change default config in flask babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+        to determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.Config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+        the / root page
+        renders basic html
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
