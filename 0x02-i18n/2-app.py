c0b4b2a5e5208e4c7b76ebbdde4f493230af1ba4
oladapsy <dapoodedeyi@yahoo.com>
2024-11-06 21:40:26 +0000

    last

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale():
     """
         to determine the best match with our supported languages.
     """
-    return request.accept_languages.best_match(app.Config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
